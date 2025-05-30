e03c5ee01ddb52304f370e661c0322ae8c91cd5a
Jordi AD <56399137+jalmenech27@users.noreply.github.com>
2025-05-30 23:25:21 +0200

    Update app_tabs.py

--- app_tabs.py
+++ app_tabs.py
@@ -109,7 +109,7 @@ def plot_bubble_anim(df: pd.DataFrame):
         animation_frame="month_year",
         animation_group="distribution_channel",
         hover_name="distribution_channel",
-        size_max=80,
+        size_max=100,
         range_x=[0, bubble_df["pct_cancel"].max() + 5],
         range_y=[0, bubble_df["lead_time"].max() + 20],
         labels={
@@ -229,7 +229,7 @@ def plot_channel_evol(df: pd.DataFrame):
         animation_frame="month_year",
         animation_group="distribution_channel",
         hover_name="distribution_channel",
-        size_max=80,
+        size_max=100,
         range_x=[0, 100],
         range_y=[bubble_df["adr_mean"].min() * 0.9, bubble_df["adr_mean"].max() * 1.1],
         labels={
